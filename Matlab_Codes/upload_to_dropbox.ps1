5cd95c5423b4ae60a424f77faf990f2bf89ae062
nanditanar <143223048+nanditanar@users.noreply.github.com>
2023-09-12 11:37:33 -0400

    Update upload_to_dropbox.ps1
    updated script to copy all files and folders in a directory and place them in a Dropbox folder.

--- Matlab_Codes/upload_to_dropbox.ps1
+++ Matlab_Codes/upload_to_dropbox.ps1
@@ -1,30 +1,16 @@
 # Specify the local directory containing the files to upload
-$localDirectory = "C:\path\to\local\directory"
+$localDirectory = "" #REPLACE WITH YOUR LOCAL DIRECTORY
 
 # Specify the Dropbox folder where you want to upload the files
-$dropboxFolder = "/BioSci-Najafi"
+$dropboxFolder = "" #REPLACE WITH DROPBOX DIRECTORY
 
-# Path to the Dropbox CLI executable
-$dropboxCLI = "C:\Program Files\Dropbox\Client\Dropbox.exe"
-
-# Dropbox access token
-$dropboxAccessToken = "sl.Bl4L7XvyXjMLZFig92k-4bD26nVliefbUPTyVniVIZTFugDNP72UYwZBq1IcSEFn2DjZFd8QralEpBDBPhhzT_XxQMg7InpNbDwg5sUSrxBKxUxJPbu9-iw_EwMvNejvPVBziCEXd_Lk8EwMEfvlGb0"
-
-# Upload files to Dropbox
-& $dropboxCLI upload $localDirectory $dropboxFolder
-
-Write-Host "Files uploaded to Dropbox at $(Get-Date)"
-
-
-'''
-# List files in the local directory
+#List files in local directory
 $files = Get-ChildItem $localDirectory
 
 # Loop through the files and copy them to Dropbox
 foreach ($file in $files) {
-    $destinationPath = Join-Path -Path $dropboxDirectory -ChildPath $file.Name
-    Copy-Item -Path $file.FullName -Destination $destinationPath -Force
+    $destinationPath = Join-Path -Path $dropboxFolder -ChildPath $file.Name
+    Copy-Item -Path $file.FullName -Destination $destinationPath -Force -Recurse
     Write-Host "Uploaded $($file.Name) to Dropbox"
 }
-'''
 #executes using: .\upload_to_dropbox.ps1
