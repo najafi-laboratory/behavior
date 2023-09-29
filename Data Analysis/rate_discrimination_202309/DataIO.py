b6c18fefa7bde02be06ee94986d2bb70adc5a504
Yicong (Hilbert) Huang <130207381+HilbertHuang@users.noreply.github.com>
2023-09-29 17:43:08 -0400

    2023.09.29

---
+++ Data Analysis/rate_discrimination_202309/DataIO.py
@@ -0,0 +1,204 @@
+import os
+import scipy.io as sio
+import numpy as np
+
+
+# read .mat to dict
+def load_mat(fname):
+    def _check_keys(d):
+        for key in d:
+            if isinstance(d[key], sio.matlab.mat_struct):
+                d[key] = _todict(d[key])
+        return d
+
+    def _todict(matobj):
+        d = {}
+        for strg in matobj._fieldnames:
+            elem = matobj.__dict__[strg]
+            if isinstance(elem, sio.matlab.mat_struct):
+                d[strg] = _todict(elem)
+            elif isinstance(elem, np.ndarray):
+                d[strg] = _tolist(elem)
+            else:
+                d[strg] = elem
+        return d
+
+    def _tolist(ndarray):
+        elem_list = []
+        for sub_elem in ndarray:
+            if isinstance(sub_elem, sio.matlab.mat_struct):
+                elem_list.append(_todict(sub_elem))
+            elif isinstance(sub_elem, np.ndarray):
+                elem_list.append(_tolist(sub_elem))
+            else:
+                elem_list.append(sub_elem)
+        return elem_list
+    data = sio.loadmat(fname, struct_as_record=False, squeeze_me=True)
+    data = _check_keys(data)
+    data = data['SessionData']
+    return data
+
+
+# extract all session data for a subject
+
+def read_trials(subject):
+
+    file_names = os.listdir(os.path.join('data', subject))
+    file_names.sort(key=lambda x: x[-19:])
+    session_raw_data = []
+    session_post_lick = []
+    session_choice = []
+    session_dates = []
+    session_outcomes = []
+    session_isi = []
+    session_avsync = []
+    LR12_start = 0
+    for f in range(len(file_names)):
+        # the first session with LR_12
+        fname = file_names[f]
+        if LR12_start==0 and fname[14:16]=='12':
+            LR12_start = f
+        # one session data
+        raw_data = load_mat(os.path.join('data', subject, fname))
+        session_raw_data.append(raw_data)
+        # number of trials
+        nTrials = raw_data['nTrials']
+        # trial target
+        TrialTypes = raw_data['TrialTypes']
+        # session date
+        session_dates.append(fname[-19:-11])
+        # loop over one session for extracting data
+        trial_post_lick = []
+        trial_outcomes = []
+        trial_isi = []
+        trial_avsync = []
+        trial_choice = []
+        for i in range(nTrials):
+            # handle key error if only one trial in a session
+            if nTrials > 1:
+                trial_states = raw_data['RawEvents']['Trial'][i]['States']
+                trial_events = raw_data['RawEvents']['Trial'][i]['Events']
+            else:
+                trial_states = raw_data['RawEvents']['Trial']['States']
+                trial_events = raw_data['RawEvents']['Trial']['Events']
+            # outcome
+            outcome = states_labeling(trial_states)
+            trial_outcomes.append(outcome)
+            # stim isi
+            event_keys = trial_events.keys()
+            if ('BNC1High' in event_keys) and ('BNC1Low' in event_keys):
+                BNC1High = trial_events['BNC1High']
+                BNC1High = np.array(BNC1High).reshape(-1)
+                BNC1Low = trial_events['BNC1Low']
+                BNC1Low = np.array(BNC1Low).reshape(-1)
+                if len(BNC1High) > 1 and len(BNC1Low) > 1:
+                    BNC1High = BNC1High[1:]
+                    BNC1Low = BNC1Low[0:-1]
+                    if np.size(BNC1High) == np.size(BNC1Low):
+                        BNC1HighLow = BNC1High - BNC1Low
+                        trial_isi.append(BNC1HighLow)
+            # trial choice
+                        if outcome in [
+                            'Reward',
+                            'RewardNaive',
+                            'Punish',
+                            'PunishNaive']:
+                            if outcome in [
+                                'Reward',
+                                'RewardNaive']:
+                                choice = TrialTypes[i]-1
+                            if outcome in [
+                                'Punish',
+                                'PunishNaive']:
+                                choice = 2-TrialTypes[i]
+                            # 0-left / 1-right
+                            trial_choice.append(np.array([BNC1HighLow[-1], choice]))
+            # post-perturbation licking events
+                        post_start_idx = np.where(
+                            np.abs(BNC1HighLow - BNC1HighLow[-1]) <= 0.05)[0]
+                        if (len(post_start_idx) > 0 and
+                            BNC1HighLow[-1] < 1.1 and
+                            BNC1HighLow[-1] > -0.1
+                            ):
+                            # early choice included
+                            post_start_idx = post_start_idx[0]
+                            lick_left = np.array([])
+                            lick_right = np.array([])
+                            if 'Port1In' in event_keys:
+                                lick_left = np.array(trial_events['Port1In']).reshape(-1)
+                                if len(lick_left) > 0:
+                                    lick_left = lick_left[post_start_idx:]
+                            if 'Port3In' in event_keys:
+                                lick_right = np.array(trial_events['Port3In']).reshape(-1)
+                                if len(lick_left) > 0:
+                                    lick_right = lick_right[post_start_idx:]
+                            right_pc = len(lick_right)/(len(lick_left)+len(lick_right)+1e-5)
+                            trial_post_lick.append(np.array([BNC1HighLow[-1], right_pc]))
+            # av sync
+            if ('BNC1High' in event_keys) and ('BNC2High' in event_keys):
+                BNC1High = trial_events['BNC1High']
+                BNC1High = np.array(BNC1High).reshape(-1)
+                BNC2High = trial_events['BNC2High']
+                BNC2High = np.array(BNC2High).reshape(-1)
+                FirstGrating = BNC1High[0]
+                FirstGratingAudioDiff = FirstGrating - BNC2High
+                FirstGratingAudioDiff_abs = abs(FirstGratingAudioDiff)
+                avsync_abs = min(FirstGratingAudioDiff_abs).tolist()
+                AudioStimStartIndex = FirstGratingAudioDiff_abs.tolist().index(avsync_abs)
+                avsync = (FirstGrating - BNC2High)[AudioStimStartIndex]
+                trial_avsync.append(avsync.tolist())
+        # save one session data
+        session_choice.append(trial_choice)
+        session_post_lick.append(trial_post_lick)
+        session_outcomes.append(trial_outcomes)
+        session_isi.append(trial_isi)
+        session_avsync.append(trial_avsync)
+    # merge all session data
+    data = {
+        'total_sessions' : len(session_outcomes),
+        'subject' : subject,
+        'filename' : file_names,
+        'LR12_start' : LR12_start,
+        'raw' : session_raw_data,
+        'dates' : session_dates,
+    	'outcomes' : session_outcomes,
+        'choice' : session_choice,
+        'post_lick' : session_post_lick,
+        'isi' : session_isi,
+        'avsync' : session_avsync,
+    }
+    return data
+
+
+# labeling every trials for a subject
+def states_labeling(trial_states):
+    if 'Reward' in trial_states.keys() and not np.isnan(trial_states['Reward'][0]):
+        outcome = 'Reward'
+    elif 'Punish' in trial_states.keys() and not np.isnan(trial_states['Punish'][0]):
+        outcome = 'Punish'
+    elif 'ExtraStimDurPostRew_Naive' in trial_states.keys() and not np.isnan(trial_states['ExtraStimDurPostRew_Naive'][0]):
+        outcome = 'RewardNaive'
+    elif 'PunishNaive' in trial_states.keys() and not np.isnan(trial_states['PunishNaive'][0]):
+        outcome = 'PunishNaive'
+    elif 'WrongInitiation' in trial_states.keys() and not np.isnan(trial_states['WrongInitiation'][0]):
+        outcome = 'WrongInitiation'
+    elif 'EarlyChoice' in trial_states.keys() and not np.isnan(trial_states['EarlyChoice'][0]):
+        outcome = 'EarlyChoice'
+    elif 'EarlyChoice_1' in trial_states.keys() and not np.isnan(trial_states['EarlyChoice_1'][0]):
+        outcome = 'EarlyChoice'
+    elif 'EarlyChoiceDurCenterLick' in trial_states.keys() and not np.isnan(trial_states['EarlyChoiceDurCenterLick'][0]):
+        outcome = 'EarlyChoice'
+    elif 'DidNotChoose' in trial_states.keys() and not np.isnan(trial_states['DidNotChoose'][0]):
+        outcome = 'DidNotChoose'
+    elif 'DidNotConfirm' in trial_states.keys() and not np.isnan(trial_states['DidNotConfirm'][0]):
+        outcome = 'DidNotConfirm'
+    elif 'DidNotLickCenter' in trial_states.keys() and not np.isnan(trial_states['DidNotLickCenter'][0]):
+        outcome = 'DidNotLickCenter'
+    else:
+        outcome = 'Other'
+    return outcome
+
+
+# get choice from outcomes and trial types
+def compute_choice():
+    0
---
+++ Data Analysis/rate_discrimination_202309/plot/fig3_percentage_epoch.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+states = [
+    'Reward',
+    'RewardNaive',
+    'Punish',
+    'PunishNaive',
+    'WrongInitiation',
+    'EarlyChoice']
+colors = [
+    'springgreen',
+    'limegreen',
+    'coral',
+    'lightcoral',
+    'orange',
+    'dodgerblue']
+
+
+def count_bin(outcomes, states, bin_size):
+    session_counts = []
+    for i in range(len(outcomes)):
+        trial_counts = []
+        num_bins = int(len(outcomes[i])/bin_size)
+        for j in range(num_bins):
+            end_idx = len(outcomes[i]) if j == num_bins-1 else (j+1)*bin_size
+            bin_data = outcomes[i][j*bin_size:end_idx]
+            trial_counts.append(count_label(bin_data, states))
+        session_counts.append(np.array(trial_counts))
+    return session_counts
+
+
+def count_label(outcomes, states):
+    counts = np.zeros(len(states))
+    for j in range(len(states)):
+        counts[j] = np.sum(np.array(outcomes) == states[j])
+    counts = counts / (np.sum(counts)+1e-5)
+    return counts
+
+
+def plot_line(axs, session_data, session_id=-1, bin_size=10):
+    subject = session_data['subject']
+    outcomes = session_data['outcomes']
+    dates = session_data['dates']
+    counts = count_bin(outcomes, states, bin_size)
+    counts = counts[session_id]
+    for j in range(len(states)):
+        if len(counts)>0:
+            axs.plot(
+                np.arange(len(counts))+1,
+                counts[:,j],
+                color=colors[j],
+                label=states[j])
+    axs.tick_params(tick1On=False)
+    axs.spines['left'].set_visible(False)
+    axs.spines['right'].set_visible(False)
+    axs.spines['top'].set_visible(False)
+    axs.yaxis.grid(True)
+    axs.set_xlabel('bins with size {}'.format(bin_size))
+    axs.set_ylabel('percentage')
+    axs.set_title(subject + ' ' + dates[session_id])
+
+
+def plot_fig3(
+    session_data_1,
+    session_data_2,
+    session_data_3,
+    session_data_4
+    ):
+    fig, axs = plt.subplots(2, 2, figsize=(20,8))
+    plt.subplots_adjust(hspace=0.7)
+    plot_line(axs[0,0], session_data_1)
+    plot_line(axs[0,1], session_data_2)
+    plot_line(axs[1,0], session_data_3)
+    plot_line(axs[1,1], session_data_4)
+    axs[0,1].legend(loc='upper left', bbox_to_anchor=(1,1), ncol=1)
+    fig.suptitle('reward/punish percentage for completed trials across epoches')
+    fig.tight_layout()
+    fig.savefig('./figures/fig3_percentage_epoch.pdf', dpi=300)
+    fig.savefig('./figures/fig3_percentage_epoch.png', dpi=300)
+
