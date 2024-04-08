6d6f4b1559d68d783a62c21b85b7d89d03b36287
Yicong (Hilbert) Huang <130207381+HilbertHuang@users.noreply.github.com>
2024-04-08 16:53:18 -0400

    Delete Data Analysis/2p_processing_pipeline_202401 directory

--- Data Analysis/2p_processing_pipeline_202401/backup/SaveOps.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import json
-from suite2p import default_ops
-
-ops = default_ops()
-
-with open("config.json", "w") as f: 
-    json.dump(ops, f)
--- Data Analysis/2p_processing_pipeline_202401/backup/modules/Denoise.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import gc
-import os
-import tifffile
-import numpy as np
-from tqdm import tqdm
-from datetime import datetime
-
