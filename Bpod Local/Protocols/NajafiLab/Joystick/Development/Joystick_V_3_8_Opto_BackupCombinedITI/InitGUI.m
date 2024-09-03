ed8bbf4e6d2dc79f3ea9b6919f37bb5675a9672d
TimStamm <gtg424h@gatech.edu>
2024-09-02 23:49:33 -0400

    Joystick_V_3_8 update
    updating v_3_8 see onenote

--- Bpod Local/Protocols/NajafiLab/Joystick/Development/Joystick_V_3_8_Opto_BackupCombinedITI/InitGUI.m
+++ Bpod Local/Protocols/NajafiLab/Joystick/Development/Joystick_V_3_8_Opto_BackupCombinedITI/InitGUI.m
@@ -9,32 +9,27 @@ function [S] = SetParams(obj, BpodSystem)
         S.GUI.currentTrial = 0;
         
         % joystick params
-        S.GUI.SelfTimedMode = 0;
-        S.GUIMeta.SelfTimedMode.Style = 'checkbox';  
         S.GUI.ChemogeneticSession = 0;
         S.GUIMeta.ChemogeneticSession.Style = 'checkbox';
         S.GUI.mgCNO = '1';
         S.GUI.mlSaline = '1';
-
-        % S.GUI.mgCNO_mlSaline = '1:1';   % number of required repeated lever presses in a trial
-        % S.GUIMeta.mgCNO_mlSaline.Style = 'text';
-
         S.GUI.Threshold = 2; % Threshold for completion of a lever press, units = degrees of shaft rotation        
         S.GUI.WarmupThreshold = S.GUI.Threshold / 2; % Threshold for completion of a lever press, units = degrees of shaft rotation        
-        S.GUI.RetractThreshold = 0.3;
-        S.GUI.EarlyPressThreshold = 1; 
+        S.GUI.EnableAutoPressWinReduce = 0;
+        S.GUIMeta.EnableAutoPressWinReduce.Style = 'checkbox';
+        S.GUI.AutoPressWinReduceStep = 0.001;
+        S.GUI.AutoPressWinReduceMin = 0.500;
         S.GUI.Press1Window_s = 1.8; % how long mouse has to press lever
         S.GUI.Press2Window_s = 1.8; % how long mouse has to press lever
-        S.GUI.PressWindowExtend_s = 1; % additional time to press lever added for warmup trials        
-        S.GUI.EnableAutoPressWinReduce = 0;
-        S.GUIMeta.EnableAutoPressWinReduce.Style = 'checkbox';        
-        S.GUI.AutoPressWinReduceMin = 0.500;
-        S.GUI.AutoPressWinReduceStep = 0.001;
-
+        S.GUI.PressWindowExtend_s = 1; % additional time to press lever added for warmup trials
         S.GUI.Reps = 2;   % number of required repeated lever presses in a trial
         S.GUIMeta.Reps.Style = 'popupmenu'; % the GUIMeta field is used by the ParameterGUI plugin to customize UI objects.
-        S.GUIMeta.Reps.String = {'1', '2', '3'};       
-
+        S.GUIMeta.Reps.String = {'1', '2', '3'};
+        S.GUI.ZeroRTrials = 1;
+        S.GUI.ResistanceLevel = 1;
+        S.GUIMeta.ResistanceLevel.Style = 'popupmenu'; % the GUIMeta field is used by the ParameterGUI plugin to customize UI objects.
+        S.GUIMeta.ResistanceLevel.String = {'0 mA', '30 mA', '91 mA', '122 mA', '152 mA', '183 mA', '214 mA', '244 mA', '900 mA'};
+        
         % set servo positions per rig, allows consistent code version
         % updates compatible across rigs
         switch BpodSystem.Data.RigName
@@ -48,22 +43,23 @@ function [S] = SetParams(obj, BpodSystem)
                 S.GUI.ServoInPos = 1598.00; % lever start pos
                 S.GUI.ServoOutPos = 34; % can press lever            
         end
+        S.GUI.RetractThreshold = 0.3;
+        S.GUI.EarlyPressThreshold = 1;
+        S.GUI.SelfTimedMode = 0;
+        S.GUIMeta.SelfTimedMode.Style = 'checkbox';  
 
         S.GUI.PrePress2DelayShort_s = 0.100;
         S.GUI.PrePress2DelayLong_s = 0.200;
 
-        S.GUI.EnableAutoDelay = 1;
-        S.GUIMeta.EnableAutoDelay.Style = 'checkbox';                
-        S.GUI.AutoDelayMaxShort_s = 0.500;
-        S.GUI.AutoDelayMaxLong_s = 1.000;
-        S.GUI.AutoDelayStep_s = 0.0003;
-
         S.GUI.EnablePreVis2DelayJitter = 0;
         S.GUIMeta.EnablePreVis2DelayJitter.Style = 'checkbox';
-        S.GUI.PreVis2DelayJitterStd = 0.080;  % std deviation of normal dist for drawing jitter
-        S.GUI.PreVis2DelayMargin_s = 0.050;    % S.GUI.PreVis2DelayMargin_s = 0.050;
-        % range cuttoff for jitter
+        S.GUI.PreVis2DelayMargin_s = 0.050;
 
+        S.GUI.EnableAutoDelay = 1;
+        S.GUIMeta.EnableAutoDelay.Style = 'checkbox';        
+        S.GUI.AutoDelayStep_s = 0.0003;
+        S.GUI.AutoDelayMaxShort_s = 0.500;
+        S.GUI.AutoDelayMaxLong_s = 1.000;
         S.GUI.EnableManualTrialType = 0;
         S.GUIMeta.EnableManualTrialType.Style = 'checkbox';
         S.GUI.ManualTrialType = 1;
@@ -75,8 +71,7 @@ function [S] = SetParams(obj, BpodSystem)
         S.GUI.NumTrialsPerBlock = 30;
         S.GUI.BlockLengthMargin = 5;
         S.GUI.ProbeTrialFraction = 0.1;
-        S.GUIPanels.Joystick = {'SelfTimedMode', 'ChemogeneticSession', 'mgCNO', 'mlSaline', 'Threshold', 'WarmupThreshold', 'RetractThreshold', 'EarlyPressThreshold', 'Press1Window_s', 'Press2Window_s', 'PressWindowExtend_s',  'EnableAutoPressWinReduce', 'AutoPressWinReduceMin', 'AutoPressWinReduceStep', 'Reps', 'ServoInPos', 'ServoOutPos', 'PrePress2DelayShort_s', 'PrePress2DelayLong_s', 'EnableAutoDelay', 'AutoDelayMaxShort_s', 'AutoDelayMaxLong_s', 'AutoDelayStep_s', 'EnablePreVis2DelayJitter', 'PreVis2DelayJitterStd', 'PreVis2DelayMargin_s', 'EnableManualTrialType', 'ManualTrialType', 'TrialTypeSequence', 'NumTrialsPerBlock', 'BlockLengthMargin', 'ProbeTrialFraction'};
-        % S.GUIPanels.Joystick = {'SelfTimedMode', 'ChemogeneticSession', 'mgCNO', 'mlSaline', 'mgCNO_mlSaline', 'Threshold', 'WarmupThreshold', 'RetractThreshold', 'EarlyPressThreshold', 'Press1Window_s', 'Press2Window_s', 'PressWindowExtend_s',  'EnableAutoPressWinReduce', 'AutoPressWinReduceMin', 'AutoPressWinReduceStep', 'Reps', 'ServoInPos', 'ServoOutPos', 'PrePress2DelayShort_s', 'PrePress2DelayLong_s', 'EnableAutoDelay', 'AutoDelayMaxShort_s', 'AutoDelayMaxLong_s', 'AutoDelayStep_s', 'EnablePreVis2DelayJitter', 'PreVis2DelayJitterStd', 'PreVis2DelayMargin_s', 'EnableManualTrialType', 'ManualTrialType', 'TrialTypeSequence', 'NumTrialsPerBlock', 'BlockLengthMargin', 'ProbeTrialFraction'};
+        S.GUIPanels.Joystick = {'ChemogeneticSession', 'mgCNO', 'mlSaline', 'Threshold', 'WarmupThreshold', 'EnableAutoPressWinReduce', 'AutoPressWinReduceStep', 'AutoPressWinReduceMin', 'Press1Window_s', 'Press2Window_s', 'PressWindowExtend_s', 'Reps', 'ZeroRTrials', 'ResistanceLevel', 'ServoInPos', 'ServoOutPos', 'RetractThreshold', 'EarlyPressThreshold', 'SelfTimedMode', 'PrePress2DelayShort_s', 'PrePress2DelayLong_s', 'EnablePreVis2DelayJitter', 'PreVis2DelayMargin_s', 'EnableAutoDelay', 'AutoDelayStep_s', 'AutoDelayMaxShort_s', 'AutoDelayMaxLong_s', 'EnableManualTrialType', 'ManualTrialType', 'TrialTypeSequence', 'NumTrialsPerBlock', 'BlockLengthMargin', 'ProbeTrialFraction'};
         % S.GUIPanels.Joystick = {'ChemogeneticSession', 'mgCNO', 'mlSaline', 'Threshold', 'WarmupThreshold', 'EnableAutoPressWinReduce', 'AutoPressWinReduceStep', 'AutoPressWinReduceMin', 'Press1Window_s', 'Press2Window_s', 'PressWindowExtend_s', 'Reps', 'ZeroRTrials', 'ResistanceLevel', 'ServoInPos', 'ServoOutPos', 'RetractThreshold', 'EarlyPressThreshold', 'SelfTimedMode', 'PrePress2DelayShort_s', 'PrePress2DelayLong_s', 'PreVis2DelayMargin_s', 'EnableAutoDelay', 'AutoDelayStep_s', 'AutoDelayMaxShort_s', 'AutoDelayMaxLong_s', 'EnableManualTrialType', 'ManualTrialType', 'TrialTypeSequence', 'NumTrialsPerBlock', 'BlockLengthMargin', 'ProbeTrialFraction'};
 
         % Optogentic params
@@ -91,25 +86,20 @@ function [S] = SetParams(obj, BpodSystem)
         S.GUI.LEDOffPulseDur_ms = 92.2;
         S.GUI.OptoVis1 = 1;
         S.GUIMeta.OptoVis1.Style = 'checkbox';
-
-        S.GUI.OptoVis2 = 1;
-        S.GUIMeta.OptoVis2.Style = 'checkbox';
-
         S.GUI.OptoWaitForPress1 = 1;
         S.GUIMeta.OptoWaitForPress1.Style = 'checkbox';
-
-
-        S.GUI.OptoWaitForPress2 = 1;
-        S.GUIMeta.OptoWaitForPress2.Style = 'checkbox';        
-
         S.GUI.OptoPress1 = 1;
         S.GUIMeta.OptoPress1.Style = 'checkbox';
 
+        S.GUI.OptoPrePressDelay = 1;
+        S.GUIMeta.OptoPrePressDelay.Style = 'checkbox';
+
+        S.GUI.OptoVis2 = 1;
+        S.GUIMeta.OptoVis2.Style = 'checkbox';
+        S.GUI.OptoWaitForPress2 = 1;
+        S.GUIMeta.OptoWaitForPress2.Style = 'checkbox';
         S.GUI.OptoPress2 = 1;
         S.GUIMeta.OptoPress2.Style = 'checkbox';
-
-        S.GUI.OptoPrePressDelay = 1;
-        S.GUIMeta.OptoPrePressDelay.Style = 'checkbox';        
 
         S.GUI.OptoTrialTypeSeq = 5; % S.GUI.OptoTrialTypeSeq = 5;
         S.GUIMeta.OptoTrialTypeSeq.Style = 'popupmenu';
@@ -118,19 +108,19 @@ function [S] = SetParams(obj, BpodSystem)
         S.GUI.NumOptoTrialsPerBlock = 50; 
         S.GUI.EpochTrialStart = 1;
         S.GUI.EpochTrialStop = 15;        
-        S.GUIPanels.Opto = {'SessionType', 'PulseType', 'MaxOptoDur_s', 'LEDOnPulseDur_ms', 'LEDOffPulseDur_ms','OptoVis1', 'OptoVis2', 'OptoWaitForPress1', 'OptoWaitForPress2', 'OptoPress1', 'OptoPress2', 'OptoPrePressDelay',  'OptoTrialTypeSeq', 'OnFraction', 'NumOptoTrialsPerBlock', 'EpochTrialStart', 'EpochTrialStop'};
+        S.GUIPanels.Opto = {'SessionType', 'PulseType', 'MaxOptoDur_s', 'LEDOnPulseDur_ms', 'LEDOffPulseDur_ms','OptoVis1', 'OptoWaitForPress1', 'OptoPress1', 'OptoPrePressDelay', 'OptoVis2', 'OptoWaitForPress2', 'OptoPress2', 'OptoTrialTypeSeq', 'OnFraction', 'NumOptoTrialsPerBlock', 'EpochTrialStart', 'EpochTrialStop'};
 
 
 
         % reward
         S.GUI.PreRewardDelay_s = 0.200;
         S.GUI.EnableAutoPreRewardDelay = 1;
-        S.GUIMeta.EnableAutoPreRewardDelay.Style = 'checkbox';                        
-        S.GUI.AutoPreRewardDelayMax_s = 0.100;
+        S.GUIMeta.EnableAutoPreRewardDelay.Style = 'checkbox';                
         S.GUI.AutoPreRewardDelayStep_s = 0.0001;
+        S.GUI.AutoPreRewardDelayMax_s = 0.100;                
         S.GUI.PostRewardDelay_s = 1.000; % post reward delay prior to lever return
         S.GUI.CenterValveAmount_uL = 1;
-        S.GUIPanels.Reward = {'PreRewardDelay_s', 'EnableAutoPreRewardDelay', 'AutoPreRewardDelayMax_s', 'AutoPreRewardDelayStep_s', 'PostRewardDelay_s', 'CenterValveAmount_uL'};        
+        S.GUIPanels.Reward = {'PreRewardDelay_s', 'EnableAutoPreRewardDelay', 'AutoPreRewardDelayStep_s', 'AutoPreRewardDelayMax_s', 'PostRewardDelay_s', 'CenterValveAmount_uL'};        
         
 
         % ITI params
