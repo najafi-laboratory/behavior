e6fa2e6fa9e47fc24c4b3750d2787c0f653f24cf
TimStamm <90421614+TimStamm@users.noreply.github.com>
2024-10-18 19:34:30 -0400

    joystick update
    V_3_9 main version

--- Bpod Local/Protocols/NajafiLab/Joystick/Development/Joystick_V_3_9_Opto/fnExcludedTrials.m
+++
@@ -1,8 +0,0 @@
-function fnExcludedTrials(obj, source,event)
-    global BpodSystem
-    global S
-    S = BpodParameterGUI('sync', S);
-    BpodSystem.Data.Excluded = S.GUI.numExcludedTrials;
-end
-
-
