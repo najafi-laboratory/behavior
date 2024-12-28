0c9440ed22f6ae8514ecd30578041562767d5e1a
TimStamm <90421614+TimStamm@users.noreply.github.com>
2024-12-27 21:53:22 -0500

    update sid v 1 5
    servo velocity gui param

---
+++ Bpod Local/Protocols/NajafiLab/rate_discrimination/single_interval_discrimination_V_1_5/SoftCodeHandler.m
@@ -0,0 +1,56 @@
+function SoftCodeHandler(code)
+    HandleVideo(code)
+    HandleMoveSpouts(code)
+end
+
+
+function HandleVideo(code)
+    global BpodSystem
+    switch true
+        case code == 255 % stop video
+            BpodSystem.PluginObjects.V.stop;
+        case code == 254 % stop video
+            BpodSystem.PluginObjects.V.stop;     
+        case code == 25 % play video
+            BpodSystem.PluginObjects.V.play(code);
+    end
+end
+
+
+function HandleMoveSpouts(code)
+    global BpodSystem
+    global AntiBiasVar
+    global S
+    global TargetConfig
+    switch S.GUI.EnableMovingSpouts
+        case 1
+            global M
+            switch true
+                case code == 254
+                    M.setMotor(0, ConvertMaestroPos(S.GUI.RightServoInPos - S.GUI.ServoDeflection), S.GUI.ServoVelocity);
+                    M.setMotor(1, ConvertMaestroPos(S.GUI.LeftServoInPos + S.GUI.ServoDeflection), S.GUI.ServoVelocity);        
+                case code == 9
+                    if AntiBiasVar.MoveCorrectSpout
+                        switch BpodSystem.Data.TrialTypes(end)
+                            case 1
+                                M.setMotor(1, ConvertMaestroPos(S.GUI.LeftServoInPos + AntiBiasVar.ServoLeftAdjust), S.GUI.ServoVelocity); 
+                            case 2
+                                M.setMotor(0, ConvertMaestroPos(S.GUI.RightServoInPos + AntiBiasVar.ServoRightAdjust), S.GUI.ServoVelocity);
+                        end
+                    else
+                        M.setMotor(1, ConvertMaestroPos(S.GUI.LeftServoInPos + AntiBiasVar.ServoLeftAdjust), S.GUI.ServoVelocity); 
+                        M.setMotor(0, ConvertMaestroPos(S.GUI.RightServoInPos + AntiBiasVar.ServoRightAdjust), S.GUI.ServoVelocity);
+                    end                                            
+                    disp(['Moving right servo to: ', num2str(S.GUI.RightServoInPos + AntiBiasVar.ServoRightAdjust)]);
+                    disp(['Moving left servo to: ', num2str(S.GUI.LeftServoInPos + AntiBiasVar.ServoLeftAdjust)]);
+            end
+        case 0
+    end
+end
+
+
+function SetMotorPos = ConvertMaestroPos(MaestroPosition)
+    m = 0.002;
+    b = -3;
+    SetMotorPos = MaestroPosition * m + b;
+end
