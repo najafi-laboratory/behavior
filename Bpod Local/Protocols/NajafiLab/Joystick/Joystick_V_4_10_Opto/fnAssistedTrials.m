ff3961ac8d08d948a26afc643cdbdfec9008a099
Yicong (Hilbert) Huang <130207381+HilbertHuang@users.noreply.github.com>
2026-08-12 15:56:02 -0400

    Delete Bpod Local/Protocols/NajafiLab/Joystick/Joystick_V_4_10_Opto directory

--- Bpod Local/Protocols/NajafiLab/Joystick/Joystick_V_4_10_Opto/fnAssistedTrials.m
+++
@@ -1,8 +0,0 @@
-function fnAssistedTrials(obj, source,event)
-    global BpodSystem
-    global S
-    S = BpodParameterGUI('sync', S);
-    BpodSystem.Data.Assisted(S.GUI.ATRangeStart:S.GUI.ATRangeStop) = 1;
-end
-
-
